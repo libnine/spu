20c4a4fc9807c9a7fc977b29dcc1226589e88c75
Matthew Gorzka <matthew_gorzka@intuit.com>
2019-10-01 10:51:27 -0700

    readme, cal

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-## Spu
+# Spu
+*A spoo is a contract that trades on the Chicago Mercantile Exchange.  It is the S&P 500 Contract.  And a spoo is the current most active month trading, either March, June, September, or December.  The symbol for the September contract is **SPU**, thus the name "spoo".*
+## A CLI for quickly retrieving relevant securities data.
+Run *spu -h* for help.
+## 
--- src/cal/cal.go
+++ src/cal/cal.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -27,23 +28,44 @@ func Calendar() {
 	e, t := events.FindAllStringSubmatch(string(body), -1), times.FindAllStringSubmatch(string(body), -1)
 
 	for i := range e {
-		detail := fmt.Sprintf("%s %s", t[i][0], e[i][1])
-		temp = append(temp, detail)
+		temp = append(temp, fmt.Sprintf("%s %s", t[i][0], e[i][1]))
 
-		if strings.Split(t[i][0], ":")[0] > strings.Split(t[i+1][0], ":")[0] && strings.Split(t[i+1][0], " ")[1] == "AM" ||
-			strings.Split(t[i][0], " ")[1] == "PM" && strings.Split(t[i+1][0], " ")[1] == "AM" {
-			fmt.Println(temp)
-			fmt.Println("YAY!")
-			time.Sleep(3 * time.Second)
+		if len(weekset) == 4 && i == len(e)-1 {
 			weekset = append(weekset, temp)
-			temp = temp[:0]
+			break
+		}
+
+		x, err := strconv.Atoi(strings.Split(t[i][0], ":")[0])
+		if err != nil {
+			log.Fatal("Error populating calendar data.")
+		}
+
+		y, err := strconv.Atoi(strings.Split(t[i+1][0], ":")[0])
+		if err != nil {
+			log.Fatal("Error populating calendar data.")
+		}
+
+		if x > y && strings.Split(t[i+1][0], " ")[1] == "AM" ||
+			strings.Split(t[i][0], " ")[1] == "PM" && strings.Split(t[i+1][0], " ")[1] == "AM" {
+			weekset = append(weekset, temp)
+			temp = nil
 		}
 	}
 
-	for n := range weekset {
-		fmt.Println(n)
+	for a := range weekset {
+		fmt.Println(weekset[a])
 	}
 
-	// str := fmt.Sprintf("%s %s", t[i][0], e[i][1])
-	// fmt.Println(str)
+	dn := int(time.Now().Weekday())
+
+	if dn == 6 || dn == 7 {
+		dn = 1
+	}
+
+	fmt.Printf("\n%s\n", time.Now().Weekday())
+	for n := range weekset[dn] {
+		fmt.Printf("\n%s", weekset[dn][n])
+	}
+
+	fmt.Printf("\n")
 }
