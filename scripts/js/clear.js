806d193d592b00f42b9db07dedd3555d9ee6d34f
Matthew Gorzka <matthew_gorzka@intuit.com>
2019-12-17 13:16:28 -0800

    clear.js fixes

--- scripts/js/clear.js
+++ scripts/js/clear.js
@@ -1,6 +1,6 @@
 const mongo = require('mongodb').MongoClient
 
-async function init(mdb) {
+async function init(dbs) {
   let client = new mongo(process.env.PFF, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -8,12 +8,23 @@ async function init(mdb) {
 
   try {
     await client.connect()
-    const db = client.db(mdb)
-    const col = db.collection("current")
-    let colarray = await col.find({}).toArray()
-    await db.collection("historical").insertMany(arr)
-    return db.collection("current").drop()
-  } 
+    let prices = []
+
+    dbs.forEach((d) => {
+        let db = client.db(d)
+        let col = db.collection("current")
+
+        prices[dbs.indexOf(d)] = col.find({}).toArray().map((p) => {
+          return p
+        })
+
+        await db.collection("current").drop() 
+        await db.collection("historical").insertMany(prices[dbs.indexOf(d)])
+    })
+
+    return true
+  }
+
   catch (e) {
     console.error(e)
   } 
@@ -23,14 +34,9 @@ async function init(mdb) {
   } 
 }
 
-init("pff")
-  .then((res) => {
-    console.log(res)
-})
-
-init("cef")
-  .then((res) => {
-    console.log(res)
+init(["pff", "cef"])
+    .then((res) => {
+        console.log(res)
 })
 
 
